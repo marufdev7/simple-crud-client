8533110d086c28ebb8f91f9a2834edeb2f53d654
mrfhmd7 <mrf.hmd7@gmail.com>
2025-06-02 17:57:44 +0600

    delete user from db

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,6 +3,19 @@ import { useLoaderData } from 'react-router-dom';
 
 const Users = () => {
     const users = useLoaderData();
+    const handleDelete = _id => {
+        console.log('delete', _id);
+        fetch(`http://localhost:5000/users/${_id}`, {
+            method: 'DELETE', 
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+                    alert('Deleted successfully');
+                }
+        })
+    }
     return (
         <div>
             <h1>{users.length}</h1>
@@ -12,6 +25,7 @@ const Users = () => {
                         {user.name}
                         :
                         {user.email}
+                        <button onClick={() => handleDelete(user._id)}>X</button>
                     </p>)
             }
         </div>
